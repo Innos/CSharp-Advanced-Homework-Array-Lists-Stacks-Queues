f0a5d4e0f3403fcf3ceae6b651e58fd15bc2247a
Innos <victor.d.kazakov@gmail.com>
2015-05-07 22:55:16 +0300

    added tasks

---
+++ 03CategorizeNumbers/CategorizeNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class CategorizeNumbers
+{
+    static void Main(string[] args)
+    {
+        List<float> input = Array.ConvertAll(Console.ReadLine().Split(), float.Parse).ToList();
+        var list1 = input.Where(x => x % (int)x == 0);
+        var list2 = input.Where(x => x % (int)x != 0);
+        Console.WriteLine("[{0}] - > min: {1}, max: {2}, sum: {3}, avg: {4:F2}"
+            , string.Join(", ", list2)
+            , list2.Min()
+            , list2.Max()
+            , list2.Sum()
+            , list2.Average());
+        Console.WriteLine("[{0}] - > min: {1}, max: {2}, sum: {3}, avg: {4:F2}"
+            , string.Join(", ", list1)
+            , list1.Min()
+            , list1.Max()
+            , list1.Sum()
+            , list1.Average());
+
+
+    }
+}
+
---
+++ 07SortedSubsetSums/SortedSubsetSums.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class SortedSubsetSums
+{
+    static void Main()
+    {
+        bool solutionFound = false;
+        int targetSum = int.Parse(Console.ReadLine());
+        int[] numbers = Console.ReadLine().Split().Select(int.Parse).Distinct().ToArray();
+        //empty iterations of i(iterations that don't pass the where and thus don't select anything) will force the first select to return a null subset 
+        //which will get written in subsets, carefull because a null entry doesn't have a first element and will cause an exception when you try to order it.
+        //Having .Where(x=>x.Sum() == targetSum is usefull as it would filter the null entries
+        var subsets = (from m in Enumerable.Range(1, (1 << numbers.Length) - 1)
+                       select
+                             (from i in Enumerable.Range(0, numbers.Length)
+                              where (m & (1 << i)) != 0
+                              select numbers[i])
+                             .ToList())
+                            .Where(x => x.Sum() == targetSum)
+                            .OrderBy(l => l.Count)
+                            .ThenBy(l => l.First())
+                            .Select(l => l.OrderBy(z => z));
+        foreach (var subset in subsets)
+        {
+            Console.WriteLine("{0} = {1}",String.Join(" + ",subset),targetSum);
+            solutionFound = true;
+        }
+        if (solutionFound == false)
+        {
+            Console.WriteLine("No matching subsets.");
+        }
+    }
+}
