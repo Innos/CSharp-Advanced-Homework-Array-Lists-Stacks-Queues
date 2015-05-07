4588028e6f5f335c89173c349778270b75bfbf52
Innos <victor.d.kazakov@gmail.com>
2015-05-08 02:19:01 +0300

    added comments

--- CSharp Advanced Homework/01Homework Arrays Lists Stacks Queues/Homework-ArraysListsStacksQueues/06SubsetSums/SubsetSums.cs
+++ CSharp Advanced Homework/01Homework Arrays Lists Stacks Queues/Homework-ArraysListsStacksQueues/06SubsetSums/SubsetSums.cs
@@ -17,6 +17,10 @@ class SubsetSums
                             from i in Enumerable.Range(0, numbers.Length)
                             where (m & (1 << i)) != 0
                             select numbers[i];
+        //LINQ Query syntaxis "from x in y" works like a foreach cycle, "where" is the same as LINQ .Where extension method,
+        // select specifies the shape of the returned element,if multiple values are given they are placed in a group, 
+        //the result of this is actually an IEnumerable<IGrouping<int>>, think of it as an unindexed List of Lists  
+        //(that doesn't have an already defined Count,but Count() method can still be called on it)
         foreach (var item in subsets)
         {
             if (item.Sum() == targetSum)
