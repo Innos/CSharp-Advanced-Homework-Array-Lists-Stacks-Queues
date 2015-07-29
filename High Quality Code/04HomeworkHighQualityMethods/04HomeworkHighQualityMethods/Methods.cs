9d5d3bee21d884595c4affa14ab480f849e636e7
Innos <victor.d.kazakov@gmail.com>
2015-07-29 04:42:19 +0300

    Updated High Quality Code Homework 4 - Optimized methods to fit with OOP principles.

--- High Quality Code/04HomeworkHighQualityMethods/04HomeworkHighQualityMethods/Methods.cs
+++ High Quality Code/04HomeworkHighQualityMethods/04HomeworkHighQualityMethods/Methods.cs
@@ -1,7 +1,6 @@
 ﻿namespace Methods
 {
     using System;
-    using System.Runtime.CompilerServices;
 
     public class Methods
     {
@@ -54,37 +53,21 @@
             return max;
         }
 
-        public static void PrintAsNumber(double number, string format)
+        public static void PrintNumberWithPrecision(double number)
         {
-            if (format == "f")
-            {
-                Console.WriteLine("{0:f2}", number);
-            }
-            if (format == "%")
-            {
-                Console.WriteLine("{0:p0}", number);
-            }
-            if (format == "r")
-            {
-                Console.WriteLine("{0,8}", number);
-            }
+            Console.WriteLine("{0:f2}", number);
         }
 
-        public static void PrintAsNumber(decimal number, string format)
+        public static void PrintNumberAsPercentage(double number)
         {
-            if (format == "f")
-            {
-                Console.WriteLine("{0:f2}", number);
-            }
-            if (format == "%")
-            {
-                Console.WriteLine("{0:p0}", number);
-            }
-            if (format == "r")
-            {
-                Console.WriteLine("{0,8}", number);
-            }
+            Console.WriteLine("{0:p0}", number);
         }
+
+        public static void PrintNumberRightAligned(double number)
+        {
+            Console.WriteLine("{0,8}", number);
+        }
+
 
         public static double CalculateDistance(
             double x1,
@@ -100,14 +83,12 @@
 
         public static bool IsHorizontal(double x1, double x2)
         {
-            bool isOnTheSameLine = x1 == x2;
-            return isOnTheSameLine;
+            return x1 == x2;
         }
 
         public static bool IsVertical(double y1, double y2)
         {
-            bool isOnTheSameLine = y1 == y2;
-            return isOnTheSameLine;
+            return y1 == y2;
         }
 
         public static void Main()
@@ -118,21 +99,21 @@
 
             Console.WriteLine(FindMaxInteger(5, -1, 3, 2, 14, 2, 3));
 
-            PrintAsNumber(1.3, "f");
-            PrintAsNumber(0.75, "%");
-            PrintAsNumber(2.30, "r");
+            PrintNumberWithPrecision(1.3);
+            PrintNumberAsPercentage(0.75);
+            PrintNumberRightAligned(2.30);
 
             Console.WriteLine(CalculateDistance(3, -1, 3, 2.5));
             Console.WriteLine("Horizontal? " + IsHorizontal(3, 3));
             Console.WriteLine("Vertical? " + IsVertical(-1, 2.5));
 
-            Student peter = new Student("Peter", "Ivanov", new DateTime(1992, 03, 17), "From Sofia, born at 17.03.1992");
+            Student peter = new Student("Peter", "Ivanov", new DateTime(1992, 03, 17), "From Sofia");
 
             Student stella = new Student(
                 "Stella",
                 "Markova",
                 new DateTime(1993, 11, 3),
-                "From Vidin, gamer, high results, born at 03.11.1993");
+                "From Vidin, gamer, high results");
 
             Console.WriteLine("{0} older than {1} -> {2}", peter.FirstName, stella.FirstName, peter.IsOlderThan(stella));
         }
