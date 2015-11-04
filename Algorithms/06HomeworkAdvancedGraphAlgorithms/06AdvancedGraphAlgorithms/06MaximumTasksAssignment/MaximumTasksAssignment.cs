594ef2a0c10678cb59667e1de89d14b810aba469
Innos <victor.d.kazakov@gmail.com>
2015-11-04 04:05:53 +0200

    changed permutations into a more optimized DFS

--- Algorithms/06HomeworkAdvancedGraphAlgorithms/06AdvancedGraphAlgorithms/06MaximumTasksAssignment/MaximumTasksAssignment.cs
+++ Algorithms/06HomeworkAdvancedGraphAlgorithms/06AdvancedGraphAlgorithms/06MaximumTasksAssignment/MaximumTasksAssignment.cs
@@ -2,68 +2,82 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Threading;
 
     public class MaximumTasksAssignment
     {
         private static bool pathFound = false;
 
-        private static bool[] visited;
+        private static bool[] tasksFinished;
 
-        private static int[,] graph;
+        private static Dictionary<int, List<int>> graph;
 
         private static int[] path;
 
         private static int tasks;
+
+        private static int counter;
 
         public static void Main(string[] args)
         {
             int persons = int.Parse(Console.ReadLine().Substring(9));
             tasks = int.Parse(Console.ReadLine().Substring(7));
 
-            graph = new int[tasks, tasks];
+            graph = new Dictionary<int, List<int>>();
             path = new int[tasks];
-            visited = new bool[tasks];
+            tasksFinished = new bool[tasks];
+            counter = 0;
 
             for (int i = 0; i < persons; i++)
             {
+                graph.Add(i, new List<int>());
                 string line = Console.ReadLine();
                 for (int j = 0; j < tasks; j++)
                 {
                     if (line[j] == 'Y')
                     {
-                        graph[i, j] = 1;
+                        graph[i].Add(j);
                     }
-
                 }
             }
-            FindAugmentingPathDFS(0);
-            for (int i = 0; i < tasks; i++)
-            {
-                Console.WriteLine("{0} -> {1}", (char)('A' + i), path[i] + 1);
-            }
-        }
 
-        static void FindAugmentingPathDFS(int loop)
-        {           
-            if (loop == tasks)
+            FindAugmentingPathDFS(0, graph);
+            if (counter < tasks)
             {
-                pathFound = true;
+                Console.WriteLine("Impossible.");
             }
             else
             {
-                for (int nextNode = 0; nextNode < tasks; nextNode++)
+                for (int i = 0; i < tasks; i++)
                 {
-                    if (!visited[nextNode] && graph[loop, nextNode] > 0)
+                    Console.WriteLine("{0} -> {1}", (char)('A' + i), path[i] + 1);
+                } 
+            }         
+        }
+
+        private static void FindAugmentingPathDFS(int node, Dictionary<int, List<int>> graph)
+        {
+            if (node == tasks)
+            {
+                pathFound = true;
+                return;
+            }
+
+            foreach (var child in graph[node])
+            {
+                if (!tasksFinished[child])
+                {
+                    tasksFinished[child] = true;
+                    path[node] = child;
+                    counter++;
+                    FindAugmentingPathDFS(node + 1, graph);
+                    tasksFinished[child] = false;
+                    if (pathFound)
                     {
-                        visited[nextNode] = true;
-                        path[loop] = nextNode;
-                        FindAugmentingPathDFS(loop + 1);
-                        visited[nextNode] = false;
-                        if (pathFound)
-                        {
-                            return;
-                        }
+                        return;
                     }
+
+                    counter--;        
                 }
             }
         }
