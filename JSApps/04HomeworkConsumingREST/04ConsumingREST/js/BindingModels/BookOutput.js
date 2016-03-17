1bbb9606b59ef8428e93b2ae28e33c03945dab34
Innos <victor.d.kazakov@gmail.com>
2016-03-17 19:24:33 +0200

    added homeworks 4-6

---
+++ JSApps/04HomeworkConsumingREST/04ConsumingREST/js/BindingModels/BookOutput.js
@@ -0,0 +1,10 @@
+var BookOutput = (function(){
+    function BookOutput(id,title,author,isbn){
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    return BookOutput;
+})();
---
+++ JSApps/06HomeworkApplicationArchitecture/06HomeworkApplicationArchitecture/js/BindingModels/BookInput.js
@@ -0,0 +1,9 @@
+var BookInput = (function(){
+    function BookInput(title,author,isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    return BookInput;
+})();
